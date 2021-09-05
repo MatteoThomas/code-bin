521ed4ba04cec566e589c9e9781e9a51603fd8db
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-09-05 12:12:09 -0600

    working, needs comments

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/Document");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/codebin", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 app.get("/", (req, res) => {
-  const code = `Welcome to Code Pile!
+  const code = `Welcome to Code Bin!
+
 Use the commands in the top right corner
 to create a new file to share with others.`;
-  res.render("code-display", { code });
+
+  res.render("code-display", { code, language: "plaintext" });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (e) {
+    // if there's an error you'll be taken back to the new page
+    res.render("new", { value });
+  }
+});
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("new", { value: document.value });
+  } catch (e) {
+    res.redirect(`/${id}`);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    // renders a new page with the text from the save
+    res.render("code-display", { code: document.value, id });
+  } catch (e) {
+    res.redirect("/");
+  }
 });
 
 app.listen(3000);
